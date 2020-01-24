2f45e3d6a19c9f7d01cf1a8f55fc3b39d8016fa1
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-24 23:07:14 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,18 +12,5 @@ end
 #
 # { directorOne => allTheMoneyTheyMade, ... }
 
-grand_total = 0
-row_index = 0
-while row_index < vm.length do
-  column_index = 0
-  while column_index < vm[row_index].length do
-    grand_total += total_value_of_spinner(vm, row_index, column_index)
-    column_index += 1
-  end
-  row_index += 1
-end
 
-def directors_totals(nds)
-  result = {}
-  nil
 end
